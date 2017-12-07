89c3c8ca70ba131db143a4e783ad0c28f4e4c203
eight <eight04@gmail.com>
2017-12-07 17:29:02 +0800

    Update pyxcute

--- cute.py
+++ cute.py
@@ -1,15 +1,11 @@
 #! python3
 
-from xcute import cute, conf, Exc, Bump, Version
-
-conf["proj_name"] = "apng"
-
-Version('{proj_name}/__init__.py')()
+from xcute import cute, conf, exc
 
 cute(
+	pkg_name = "apng",
 	test = 'readme_build',
 	bump_pre = 'test',
-	bump = Bump("{proj_name}/__init__.py"),
 	bump_post = ['dist', 'release', 'publish', 'install'],
 	dist = 'python setup.py sdist bdist_wheel',
 	release = [
@@ -21,13 +17,12 @@ cute(
 		'twine upload dist/*{version}[.-]*',
 		'git push --follow-tags'
 	],
-	publish_err = 'start https://pypi.python.org/pypi/{proj_name}/',
+	publish_err = 'start https://pypi.python.org/pypi/{pkg_name}/',
 	install = 'pip install -e .',
 	install_err = 'elevate -c -w pip install -e .',
 	readme_build = 'python setup.py --long-description > %temp%/ld && rst2html --no-raw --exit-status=1 --verbose %temp%/ld %temp%/ld.html',
-	readme_build_err = ['readme_show', Exc()],
+	readme_build_err = ['readme_show', exc],
 	readme_show = 'start %temp%/ld.html',
 	readme = 'readme_build',
-	readme_post = 'readme_show',
-	version = 'echo {version}'
+	readme_post = 'readme_show'
 )
