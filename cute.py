cdbc90a2de455aae2184d8437cf0d53e8c360948
eight <eight04@gmail.com>
2018-05-13 03:53:39 +0800

    Build universal wheel

--- cute.py
+++ cute.py
@@ -23,19 +23,21 @@ cute(
 	],
 	bump_pre = 'test',
 	bump_post = ['dist', 'release', 'publish', 'install'],
-	dist = 'rm -r build dist & python setup.py sdist bdist_wheel',
+	clean = "x-clean build dist",
+	dist = [
+		"clean",
+		"python setup.py sdist bdist_wheel"
+	],
 	release = [
 		'git add .',
 		'git commit -m "Release v{version}"',
 		'git tag -a v{version} -m "Release v{version}"'
 	],
 	publish = [
-		'twine upload dist/*{version}[.-]*',
+		'twine upload dist/*',
 		'git push --follow-tags'
 	],
-	publish_err = 'start https://pypi.python.org/pypi/{pkg_name}/',
 	install = 'pip install -e .',
-	install_err = 'elevate -c -w pip install -e .',
 	readme_build = [
 		'python setup.py --long-description | x-pipe build/readme/index.rst',
 		'rst2html5.py --no-raw --exit-status=1 --verbose '
